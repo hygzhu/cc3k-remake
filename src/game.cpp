709e6ebf13f56db6a14421287ab825e48850c312
hygzhu <hygzhu@gmail.com>
2023-12-23 22:53:01 -0500

    small refactor

--- src/game.cpp
+++ src/game.cpp
@@ -26,98 +26,18 @@ Game::Game(/* args */) {
 
 int Game::start() {
 
-  std::cout << "Game start" << std::endl;
-  auto player = EntityFactory::createPlayer();
-  int max_rooms = 20;
-  m_map = std::make_shared<Map>(player, max_rooms, 40, 35);
+  
   bool quit = false;
-  SDL_Event event;
+  
+  // if(!menu()){
+  //   return 0;
+  // }
 
-  const int fps = 60;
-  const int frameDelay = 1000 / fps;
-  Uint32 frameStart;
-  int frameTime;
-  int ticks = 0;
+  return gameplay();
 
-  bool downKeys[SDL_NUM_SCANCODES] = {false};
-  while (!quit) {
-
-    if (!player->getStatus().isAlive()) {
-      // player died
-      return 1;
-    }
-
-    frameStart = SDL_GetTicks();
-
-    const int speed = 160;
-    std::pair<int, int> new_location;
-    while (SDL_PollEvent(&event)) {
-      switch (event.type) {
-      case SDL_QUIT:
-        quit = true;
-        break;
-      case SDL_KEYDOWN:
-        downKeys[event.key.keysym.scancode] = true;
-        break;
-      case SDL_KEYUP:
-        downKeys[event.key.keysym.scancode] = false;
-        break;
-      }
-    }
-
-    if (downKeys[SDL_SCANCODE_R]) {
-      return true;
-    }
-
-    if (downKeys[SDL_SCANCODE_A]) {
-      player->getMovement()->getAccelX() = -speed;
-    }
-    if (downKeys[SDL_SCANCODE_D]) {
-
-      player->getMovement()->getAccelX() = speed;
-    }
-    if ((!downKeys[SDL_SCANCODE_D] && !downKeys[SDL_SCANCODE_A]) ||
-        downKeys[SDL_SCANCODE_D] && downKeys[SDL_SCANCODE_A]) {
-      player->getMovement()->getAccelX() = 0;
-    }
-
-    if (downKeys[SDL_SCANCODE_W]) {
-      player->getMovement()->getAccelY() = -speed;
-    }
-    if (downKeys[SDL_SCANCODE_S]) {
-      // Move player down
-
-      player->getMovement()->getAccelY() = speed;
-    }
-    if ((!downKeys[SDL_SCANCODE_W] && !downKeys[SDL_SCANCODE_S]) ||
-        downKeys[SDL_SCANCODE_W] && downKeys[SDL_SCANCODE_S]) {
-      player->getMovement()->getAccelY() = 0;
-    }
-
-    // Update all movable entity location
-    double frame_delay = static_cast<double>(frameDelay) / 1000;
-    m_map->updateAllMovableEntityLocations(frame_delay);
-
-    render();
-
-    // Control game loop timing
-    frameTime = SDL_GetTicks() - frameStart;
-
-    if (frameDelay > frameTime) {
-      ticks++;
-      SDL_Delay(frameDelay - frameTime);
-    }
-
-    if (ticks == fps) {
-      ticks = 0;
-      std::cout << "Map entities " << m_map->getAllEntities().size()
-                << std::endl;
-    }
-  }
-  return 0;
 }
 
-void Game::render() {
+void Game::renderGame() {
 
   const int RENDER_WIDTH = 1280;
   const int RENDER_HEIGHT = 720;
@@ -154,8 +74,183 @@ void Game::render() {
   SDL_RenderPresent(m_renderer);
 }
 
+
+void Game::renderMenu() {
+
+  const int RENDER_WIDTH = 1280;
+  const int RENDER_HEIGHT = 720;
+  const int RENDER_X = (SCREEN_WIDTH / 2) - (RENDER_WIDTH / 2);
+  const int RENDER_Y = (SCREEN_HEIGHT / 2) - (RENDER_HEIGHT / 2);
+
+  SDL_SetRenderDrawColor(m_renderer, 0, 0, 0, 255);
+  SDL_RenderClear(m_renderer);
+
+  Rectangle viewbox = Rectangle(0,0,RENDER_WIDTH,RENDER_HEIGHT);
+
+  Renderer game_window_renderer(
+      m_renderer, Rectangle(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT),
+      Rectangle(RENDER_X, RENDER_Y, RENDER_WIDTH, RENDER_HEIGHT), viewbox);
+
+
+  SDL_RenderPresent(m_renderer);
+}
+
 Game::~Game() {
   SDL_DestroyRenderer(m_renderer);
   SDL_DestroyWindow(m_window);
   SDL_Quit();
+}
+
+
+int Game::menu(){
+  
+
+  // m_map = std::make_shared<Menu>();
+
+  std::cout << "menu start" << std::endl;
+  SDL_Event event;
+  const int fps = 60;
+  const int frameDelay = 1000 / fps;
+  Uint32 frameStart;
+  int frameTime;
+  int ticks = 0;
+
+  bool downKeys[SDL_NUM_SCANCODES] = {false};
+  bool quit = false;
+  // core menu loop
+    while (!quit) {
+    frameStart = SDL_GetTicks();
+
+    const int speed = 160;
+    std::pair<int, int> new_location;
+    while (SDL_PollEvent(&event)) {
+      switch (event.type) {
+      case SDL_QUIT:
+        return 0;
+      case SDL_KEYDOWN:
+        downKeys[event.key.keysym.scancode] = true;
+        break;
+      case SDL_KEYUP:
+        downKeys[event.key.keysym.scancode] = false;
+        break;
+      }
+    }
+    if (downKeys[SDL_SCANCODE_R]) {
+      return true;
+    }
+    if (downKeys[SDL_SCANCODE_A]) {
+    }
+    if (downKeys[SDL_SCANCODE_D]) {
+    }
+    if ((!downKeys[SDL_SCANCODE_D] && !downKeys[SDL_SCANCODE_A]) ||
+        downKeys[SDL_SCANCODE_D] && downKeys[SDL_SCANCODE_A]) {
+    }
+    if (downKeys[SDL_SCANCODE_W]) {
+    }
+    if (downKeys[SDL_SCANCODE_S]) {
+    }
+    if ((!downKeys[SDL_SCANCODE_W] && !downKeys[SDL_SCANCODE_S]) ||
+        downKeys[SDL_SCANCODE_W] && downKeys[SDL_SCANCODE_S]) {
+    }
+
+    renderMenu();
+
+    // Control game loop timing
+    frameTime = SDL_GetTicks() - frameStart;
+    if (frameDelay > frameTime) {
+      ticks++;
+      SDL_Delay(frameDelay - frameTime);
+    }
+    if (ticks == fps) {
+      ticks = 0;
+    }
+  }
+  return 0;
+}
+
+int Game::gameplay(){
+  
+
+  std::cout << "Game start" << std::endl;
+  int max_rooms = 20;
+  SDL_Event event;
+  const int fps = 60;
+  const int frameDelay = 1000 / fps;
+  Uint32 frameStart;
+  int frameTime;
+  int ticks = 0;
+  auto player = EntityFactory::createPlayer();
+  m_map = std::make_shared<Map>(player, max_rooms, 40, 35);
+  bool downKeys[SDL_NUM_SCANCODES] = {false};
+  bool quit = false;
+  // core game loop
+    while (!quit) {
+
+    if (!player->getStatus().isAlive()) {
+      // player died
+      return 1;
+    }
+
+    frameStart = SDL_GetTicks();
+
+    const int speed = 160;
+    std::pair<int, int> new_location;
+    while (SDL_PollEvent(&event)) {
+      switch (event.type) {
+      case SDL_QUIT:
+        return 0;
+      case SDL_KEYDOWN:
+        downKeys[event.key.keysym.scancode] = true;
+        break;
+      case SDL_KEYUP:
+        downKeys[event.key.keysym.scancode] = false;
+        break;
+      }
+    }
+    if (downKeys[SDL_SCANCODE_R]) {
+      return true;
+    }
+    if (downKeys[SDL_SCANCODE_A]) {
+      player->getMovement()->getAccelX() = -speed;
+    }
+    if (downKeys[SDL_SCANCODE_D]) {
+      player->getMovement()->getAccelX() = speed;
+    }
+    if ((!downKeys[SDL_SCANCODE_D] && !downKeys[SDL_SCANCODE_A]) ||
+        downKeys[SDL_SCANCODE_D] && downKeys[SDL_SCANCODE_A]) {
+      player->getMovement()->getAccelX() = 0;
+    }
+    if (downKeys[SDL_SCANCODE_W]) {
+      player->getMovement()->getAccelY() = -speed;
+    }
+    if (downKeys[SDL_SCANCODE_S]) {
+      // Move player down
+      player->getMovement()->getAccelY() = speed;
+    }
+    if ((!downKeys[SDL_SCANCODE_W] && !downKeys[SDL_SCANCODE_S]) ||
+        downKeys[SDL_SCANCODE_W] && downKeys[SDL_SCANCODE_S]) {
+      player->getMovement()->getAccelY() = 0;
+    }
+
+    // Update all movable entity location
+    double frame_delay = static_cast<double>(frameDelay) / 1000;
+    m_map->updateAllMovableEntityLocations(frame_delay);
+
+    renderGame();
+
+    // Control game loop timing
+    frameTime = SDL_GetTicks() - frameStart;
+
+    if (frameDelay > frameTime) {
+      ticks++;
+      SDL_Delay(frameDelay - frameTime);
+    }
+
+    if (ticks == fps) {
+      ticks = 0;
+      std::cout << "Map entities " << m_map->getAllEntities().size()
+                << std::endl;
+    }
+  }
+  return 0;
 }
--- src/game.h
+++ src/game.h
@@ -12,10 +12,14 @@ public:
   ~Game();
 
 private:
-  void render();
+  int menu(); 
+  int gameplay(); 
+  void renderMenu();
+  void renderGame();
   SDL_Window *m_window;
   SDL_Renderer *m_renderer;
   std::shared_ptr<Map> m_map;
+// std::shared_ptr<Menu> m_menu;
 };
 
 #endif
